d198c0b561f5de04eb8ba24393038cd101c75c99
brainsmarts <59645655+brainsmarts@users.noreply.github.com>
2024-06-26 14:15:33 -0400

    refactor: adding tile measurement for consistancy (#4)

---
+++ My project/Assets/Scripts/GameLogic/UnitUtilities.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnitUtilities 
+{
+    //measurement of one tile
+    public const float TILE = 0.16f;
+}
